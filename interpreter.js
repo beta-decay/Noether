7993d66250308861d6f5742c94d3ad1bcfd7abb0
Beta Decay <ck0@btinternet.com>
2017-06-02 20:05:29 +0100

    Added more functions

--- interpreter.js
+++ interpreter.js
@@ -5,6 +5,19 @@ var ifstage, testval, test
 var functions;
 var funccode;
 
+String.prototype.shuffle = function () {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
+
 function check() {
 	currChar = code[position];
 	if (currChar == "P") {
@@ -31,17 +44,42 @@ function check() {
 	} else if (currChar == "-") {
 		A = stack.pop();
 		B = stack.pop();
-		stack.push(B-A);
+
+		if (typeof B == "string" && typeof A != "string") {
+			stack.push(B.slice(0, A) + B.slice(A+1, B.length));
+		} else if (typeof B == "string" && typeof A == "string") {
+			var regex = new RegExp(A,'g');
+			stack.push(B.replace(regex,''));
+		} else {
+			stack.push(B-A);
+		}
+
 		position += 1;
 	} else if (currChar == "*") {
 		A = stack.pop();
 		B = stack.pop();
-		stack.push(B+A);
+		if (typeof B == "string") {
+			stack.push(B.repeat(A));
+		} else {
+			stack.push(B*A);
+		}
 		position += 1;
 	} else if (currChar == "/") {
 		A = stack.pop();
 		B = stack.pop();
-		stack.push(B/A);
+		
+		if (typeof B == "string" && typeof A != "string") {
+			stack.push(B[A]);
+		} else if (typeof B == "string" && typeof A == "string") {
+			if (B.indexOf(A) === -1) {
+				stack.push(0);
+			} else {
+				stack.push(1);
+			}
+		} else {
+			stack.push(B/A);
+		}
+
 		position += 1;
 	} else if (currChar == "^") {
 		A = stack.pop();
@@ -109,6 +147,94 @@ function check() {
 		stack.push(stdin);
 		
 		position += 1
+	} else if (currChar == "_") {
+		A = stack.pop();
+		if (typeof A == "string") {
+			stack.push(A.toLowerCase());
+		} else {
+			stack.push(Math.floor(A));
+		}
+
+		position += 1;
+	} else if (currChar == "U") {
+		A = stack.pop();
+		if (typeof A == "string") {
+			stack.push(A.toUpperCase());
+		} else {
+			stack.push(Math.ceil(A));
+		}
+
+		position += 1;
+	} else if (currChar == "R") {
+		A = stack.pop();
+		if (typeof A == "string") {
+			stack.push(A.shuffle());
+		} else {
+			stack.push(Math.floor(Math.random()*A+0.5));
+		}
+
+		position += 1;
+	} else if (currChar == "@") {
+		output = '';
+		position += 1;
+	} else if (currChar == ";") {
+		A = stack.pop();
+		if (typeof A != "string") {
+			var delaypos = position+1;
+			setTimeout(function(){position = delaypos;runCode();}, delay*1000);
+			return code.length*2;
+		} else {
+			stack.push(A);
+		}
+		position += 1;
+	} else if (currChar == "D") {
+		A = stack.pop();
+		if (typeof A != 'string') {
+			var date = new Date();
+			if (A === 0) {
+				num = date.getSeconds();
+			} else if (A == 1) {
+				num = date.getMinutes();
+			} else if (A == 2) {
+				num = date.getHours();
+			} else if (A == 3) {
+				num = date.getDate();
+			} else if (A == 4) {
+				num = date.getMonth();
+			} else if (A == 5) {
+				num = date.getFullYear();
+			} else if (A == 6) {
+				num = date.toISOString();
+			} else {
+				num = date.getTime();
+			}
+			stack.push(num);
+		}
+		position += 1;
+	} else if (currChar == "S") {
+		A = stack.pop();
+		B = stack.pop();
+		C = stack.pop();
+
+		if (typeof C == "string" && typeof B != "string" && typeof A != "string") {
+			stack.push(C.slice(B,A));
+		} else {
+			stack.push(C);
+			stack.push(B);
+			stack.push(A);
+		}
+
+		position += 1;
+	} else if (currChar == "L") {
+		A = stack.pop();
+
+		if (typeof A == "string") {
+			stack.push(A.length);
+		} else {
+			stack.push(Math.log10(A));
+		}
+
+		position += 1;
 	} else if (currChar == "(") {
 		exitcondition.push(stack.pop());
 		temppos.push(position + 1);
@@ -189,7 +315,6 @@ function runCode() {
 	while(position < code.length){
 		position = check();
 	}
-	console.log(position);
 }
 
 function interpreter() {
